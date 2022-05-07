c51329240077a845fb84fca9aa17572fdf5e473c
Finn Greenstone <dfgreenstone@uchicago.edu>
2022-05-06 20:12:51 -0400

    Added support for other sizes

--- CoordsGenerator.py
+++ CoordsGenerator.py
@@ -4,20 +4,21 @@ import random
 from random import randrange
 import math
 
-def main():
-	N = 200
-	coords = []
-	for x in range(401):
-		coords.append(x)
+def inputs(N):
+        coords = []
+        for x in range(401):
+                coords.append(x)
 	#print(coords)
-	print(N)
-	for x in range(200):
-		currCoordsX = random.choice(coords)
-		coords.remove(currCoordsX)
-		currCoordsY = random.choice(coords)
-		coords.remove(currCoordsY)
+        print(N)
+        tfactor = (200 / N)
+        for x in range(N):
+                currCoordsX = random.choice(coords)
+                currCoordsY = random.choice(coords)
+                coords.remove(currCoordsY)
+                if(currCoordsX in coords):
+                        coords.remove(currCoordsX)
 
-		#o = random.randint(0, 1240)
+		#o = random.randint(0,1240)
 		#c = random.randint(o, (o+200))
 
 		#u = random.randint(1, 100)
@@ -25,23 +26,34 @@ def main():
 
 		#t = random.randint(1, 25)
 
-		euclidianDistance = math.ceil(math.sqrt(pow((currCoordsX - 200), 2) + pow((currCoordsY - 200), 2)))
+                euclidianDistance = math.ceil(math.sqrt(pow((currCoordsX - 200), 2) + pow((currCoordsY - 200), 2)))
 
-		o = euclidianDistance
-		c = euclidianDistance * 2
+                o = euclidianDistance
+                c = euclidianDistance * 2
 
-		u = euclidianDistance
-		u *= 10
+                u = euclidianDistance
+                u *= 10
 
-		t = random.randint(1, 30)
+                t = math.floor(random.randint(1, 30) * tfactor)
 
-		#print(x, end = " ")
-		print(currCoordsX, end = " ")
-		print(currCoordsY, end = " ")
-		print(o, end = " ")
-		print(c, end = " ")
-		print(u, end = " ")
-		print(t)
+                #print(x, end = " ")
+                print(currCoordsX, end = " ")
+                print(currCoordsY, end = " ")
+                print(o, end = " ")
+                print(c, end = " ")
+                print(u, end = " ")
+                print(t)
+
+def main():
+        inputs(200)
+        inputs(200)
+        inputs(200)
+        inputs(60)
+        inputs(60)
+        inputs(60)
+        inputs(25)
+        inputs(25)
+        inputs(25)
 
 
 	
@@ -49,4 +61,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+        main()
